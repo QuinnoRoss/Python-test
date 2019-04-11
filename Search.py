c37a55ddb601b903d0ec6fa8bcaa097896d5988b
kentootz <vquinno@yahoo.com>
2019-04-11 15:47:10 +0800

    edited for assorting

--- Search.py
+++ Search.py
@@ -5,19 +5,16 @@ driver = webdriver.Firefox(executable_path=r'/usr/local/bin/geckodriver');
 
 driver.get("https://carmudi.com.ph")
 
-element = driver.find_element_by_xpath('//*[@id="c-budget-container-cars"]/div[1]')
-assert "500" in element.text
+assert driver.page_source.find(" <  "u"\u20B1" "500,000 ")
+assert driver.page_source.find("  "u"\u20B1" "500,000 -  "u"\u20B1" "1,000,000 ")
+assert driver.page_source.find("  "u"\u20B1" "500,000 -  "u"\u20B1" "1,000,000 ")
+assert driver.page_source.find("> "u"\u20B1""2,000,000 ")
 
-sec_element = driver.find_element_by_xpath('//*[@id="c-budget-container-cars"]/div[2]')
-assert "1,000,000" in sec_element.text
+print ('Successfully asserted')
 
-th_element = driver.find_element_by_xpath('//*[@id="c-budget-container-cars"]/div[3]')
-assert "2,000,000" in th_element.text
 
-f_element = driver.find_element_by_xpath('//*[@id="c-budget-container-cars"]/div[4]')
-assert ">" in f_element.text
 
-print ('Successfully asserted '+element.text) 
-print ('Successfully asserted '+sec_element.text)
-print ('Successfully asserted '+th_element.text)
-print ('Successfully asserted '+f_element.text)
+
+
+
+
